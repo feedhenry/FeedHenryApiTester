04959672dd9367b976cb9480a0b2736cdfffbfd5
testing.dev@example.com <testing.dev@example.com>
2014-03-06 13:34:40 +0000

    update file /client/default/scripts/env.js

--- client/default/scripts/env.js
+++ client/default/scripts/env.js
@@ -1,6 +1,9 @@
 function showEnv(){
   $fh.env({}, function(env){
     $('#env_details').html('<p>' + JSON.stringify(env) + '</p>').show();
+    if(window.device){
+      $('#env_details').append('<p> Cordova: ' + JSON.stringify(window.device) + '</p>');
+    }
   }, function(err){
     alert("$fh.env failed!");
   });
